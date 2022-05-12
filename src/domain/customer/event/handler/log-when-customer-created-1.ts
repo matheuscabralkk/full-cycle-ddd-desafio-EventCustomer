394958580f2d6ca0874dc1a03cb457d61c0c6f90
Matheus <matheuscabralkk@gmail.com>
2022-05-11 22:25:29 -0300

    first commit

---
+++ src/domain/customer/event/handler/log-when-customer-created-1.ts
@@ -0,0 +1,10 @@
+import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
+import CustomerCreated1Event from '../customer-created-1.event';
+
+export default class LogWhenCustomerCreated1
+    implements EventHandlerInterface<CustomerCreated1Event> {
+    handle(event: CustomerCreated1Event): void {
+        // tslint:disable-next-line:no-console
+        console.log(`Esse é o primeiro console.log do evento: CustomerCreated`);
+    }
+}
---
+++ src/domain/customer/event/handler/log-when-customer-created-2.ts
@@ -0,0 +1,10 @@
+import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
+import CustomerCreated2Event from '../customer-created-2.event';
+
+export default class LogWhenCustomerCreated2
+    implements EventHandlerInterface<CustomerCreated2Event> {
+    handle(event: CustomerCreated2Event): void {
+        // tslint:disable-next-line:no-console
+        console.log(`Esse é o segundo console.log do evento: CustomerCreated`);
+    }
+}
